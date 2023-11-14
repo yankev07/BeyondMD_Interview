24933eb6c3340c80e926b7f017fe29c05def7dd3
Kevin Yanogo <kevin.yanogo@gmail.com>
2023-11-14 01:28:40 -0500

    sys_health updated

--- sys_health.sh
+++ sys_health.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "CPU Usage: $(top -bn1 | grep 'Cpu(s)' | awk '{print $2}' | awk -F. '{print $1}')%"
-echo "Memory Usage: $(free | awk '/Mem/{printf "%.2f%", $3/$2*100}')"
+echo "Memory Usage: $(free | awk '/Mem/{printf "%.2f%%", $3/$2*100}')"
 echo "Disk Usage: $(df -h | awk '$NF=="/"{printf "%s", $5}')"
 echo "Active Users: $(who | wc -l)"
 echo "Running Processes: $(ps aux | wc -l)"
